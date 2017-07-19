086b2cb1ad16cf74039702fae277f9218b1bd1a3
Moshe Nahmias <moshegrey@gmail.com>
2017-07-19 17:01:28 +0300

    removing for now login_requireed and adding static_root

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 from .views import SearchView, ResultsView
-from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    url(r'^results/(?P<pk>[0-9]+)/$', login_required(ResultsView.as_view()), name='results_page'),
+    url(r'^results/(?P<pk>[0-9]+)/$', ResultsView.as_view(), name='results_page'),
     url(r'', SearchView.as_view(), name='search_page'),
 ]
