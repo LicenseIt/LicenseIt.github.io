f7a28a3919a116077b6316b133e4fcaa81555fe9
moshe nahmias <moshegrey@gmail.com>
2017-08-23 15:30:40 +0300

    adding picture test

--- accounts/templatetags/license_it_user_image.py
+++ accounts/templatetags/license_it_user_image.py
@@ -5,6 +5,6 @@ from accounts.models import Notifications, UserImage
 register = template.Library()
 
 
-@register.simple_tag
+@register.simple_tag(is_safe=True)
 def user_image(user):
     return '<img src="{0}" alt="user image" width=40 height=40>'.format(UserImage.objects.get(user=user).image_url)
