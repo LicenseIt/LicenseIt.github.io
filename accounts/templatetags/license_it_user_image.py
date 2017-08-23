fd9f145be0880c9b21dad50c2253594a93bead94
moshe nahmias <moshegrey@gmail.com>
2017-08-23 15:35:22 +0300

    adding picture test

--- accounts/templatetags/license_it_user_image.py
+++ accounts/templatetags/license_it_user_image.py
@@ -7,4 +7,4 @@ register = template.Library()
 
 @register.simple_tag(is_safe=True)
 def user_image(user):
-    return '<img src="{0}" alt="user image" width=40 height=40>'.format(UserImage.objects.get(user=user).image_url)
+    return UserImage.objects.get(user=user).image_url
