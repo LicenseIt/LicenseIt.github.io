2de804084d63a4a7b1e1108c7026df74a889fa08
moshe nahmias <moshegrey@gmail.com>
2017-08-23 15:22:43 +0300

    adding picture test

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AskUser, Notifications
+from .models import AskUser, Notifications, UserImage
 
 # Register your models here.
 admin.site.register(AskUser)
 admin.site.register(Notifications)
+admin.site.register(UserImage)
