b832921980d564b2d99d435a244c5ed662e8e3b9
Moshe Nahmias <moshegrey@gmail.com>
2017-08-29 19:32:26 +0300

    adding reset password table to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AskUser, Notifications, UserImage
+from .models import AskUser, Notifications, UserImage, ResetPassword
 
 # Register your models here.
 admin.site.register(AskUser)
 admin.site.register(Notifications)
 admin.site.register(UserImage)
+admin.site.register(ResetPassword)
