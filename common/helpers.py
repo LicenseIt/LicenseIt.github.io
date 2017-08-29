be92308a9c0f60867fc710746d53548622cd8683
Moshe Nahmias <moshegrey@gmail.com>
2017-08-29 19:49:34 +0300

    fixing bug with reset password

--- common/helpers.py
+++ common/helpers.py
@@ -3,7 +3,10 @@ import random
 
 def generate_password(length=10):
     ret = ''
-    letters = [chr(c) for c in range(ord('A'), ord('z') + 1)]
+    letters = [chr(c) for c in range(ord('A'), ord('Z') + 1)]
+    small_letters = [chr(c) for c in range(ord('a'), ord('z') + 1)]
+    letters.extend(small_letters)
+
     for num in range(length):
         ret += random.choice(letters)
     return ret
