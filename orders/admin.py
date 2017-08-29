69f3b6a820dde36648600578dedfe98e47da9086
Moshe Nahmias <moshegrey@gmail.com>
2017-08-29 22:28:02 +0300

    enabling sending mail through admin interface

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.mail import send_mail
+
 from .models import (
     Order,
     WebEntry,
@@ -20,8 +22,24 @@ from .models import (
 )
 from .models import ProjectType
 
+
 # Register your models here.
-admin.site.register(Order)
+class MailChangeStatus(admin.ModelAdmin):
+    list_display = ['user', 'song_title', 'state', 'project_type']
+    actions = ['update_status_user']
+
+    def update_status_user(self, request, queryset):
+        for order in queryset.all():
+            send_mail('licenseit- your order status has changed',
+                      'your order status has changed to {0}. you can check it at {1}/accounts/my_account/'
+                      .format(order.state, request.get_host()),
+                      'cdo@licenseit.net',
+                      [order.user.email])
+
+    update_status_user.short_description = 'send email to update on status'
+
+
+admin.site.register(Order, MailChangeStatus)
 admin.site.register(WebEntry)
 admin.site.register(ExternalEntry)
 admin.site.register(OrderDistributionProgramming)
