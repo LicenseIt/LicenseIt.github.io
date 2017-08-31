e81be3f82620ef7f2b9a011747964cf3f13dfcbd
Moshe Nahmias <moshegrey@gmail.com>
2017-08-31 14:00:09 +0300

    adding social auth django

--- license_it/settings.py
+++ license_it/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_auth',
+    # 'social_auth',
     'social_django',
     'search',
     'accounts',
