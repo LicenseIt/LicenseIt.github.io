7cf7f6ce0d059f7d0d207a8138b5c62764bb2431
Moshe Nahmias <moshegrey@gmail.com>
2017-10-18 11:14:27 +0300

    tmp

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from datetime import datetime, timedelta
 
 from django.shortcuts import render
 from django.urls import reverse
@@ -9,7 +11,29 @@ from orders.models import Order
 
 
 # Create your views here.
-class CreatePayment(View):
+class BasePayment(View):
+    base_url = 'https://api.sandbox.paypal.com/v1/'
+
+    def get_access_token(self, request, base_url):
+        if 'expires_at' in request.session.keys() and \
+                datetime.now() > request.session['expires_at'] + request.session['last_token'] or \
+                'expires_at' not in request.session.keys():
+            access_headers = {
+                'Accept': 'application/json',
+                'Accept-Language': 'en_US',
+            }
+
+            auth = {os.environ.get('PAYPAL_APP_ID'): os.environ.get('PAYPAL_SECRET')}
+
+            auth_result = requests.get(self.base_url + '/oauth2/token',
+                                       auth=auth,
+                                       headers=access_headers)
+            result_json = auth_result.json()
+            request.session['access_token'] = result_json['access_token']
+            request.session['expires_at'] = timedelta(seconds=result_json['expires_in'])
+
+
+class CreatePayment(BasePayment):
     def post(self, request, order_id=None):
         order_price = Order.objects.get(pk=order_id).price
         paypal = {
@@ -45,18 +69,18 @@ class CreatePayment(View):
 
         headers = {
             'Content-Type': 'application/json',
-            'Authorization': 'access_token$production$6xzb7s8y9ngf8mh9$76e737512b53650362681e5022a93946'
+            'Authorization': 'Bearer'
         }
 
-        res = requests.post('https://api.sandbox.paypal.com/v1/payments/payment',
+        res = requests.post(self.base_url + 'payments/payment',
                             data=paypal,
                             headers=headers)
         return JsonResponse(res.json)
 
 
-class ExecutePayment(View):
+class ExecutePayment(BasePayment):
     def post(self, request):
-        url = 'https://api.sandbox.paypal.com/v1/payments/payment/{0}/execute/'.format(request.POST['paymentID'])
+        url = self.base_url + 'payments/payment/{0}/execute/'.format(request.POST['paymentID'])
 
         payment = {
             'payer_id': request.POST['payerID']
