dbbec43a87e7bf91fdec03cd3daed2b958dc0d56
moshe nahmias <moshegrey@gmail.com>
2017-10-22 16:02:51 +0300

    token

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from .views import CreatePayment
+from .views import CreatePayment, ExecutePayment
 
 urlpatterns = [
     url('^create-payment/(?P<order_id>[0-9]+)', login_required(CreatePayment.as_view()), name='create_payment'),
+    url('^execute-payment/(?P<order_id>[0-9]+)', login_required(ExecutePayment.as_view()), name='execute_payment'),
 ]
