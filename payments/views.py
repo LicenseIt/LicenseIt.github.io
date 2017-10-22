28db451b0e91b46dffa408e92600276342ba2c42
moshe nahmias <moshegrey@gmail.com>
2017-10-22 11:39:24 +0300

    token

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,5 @@
 from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
 from requests.auth import HTTPBasicAuth
 import os
 from datetime import datetime, timedelta
