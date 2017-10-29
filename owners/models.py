9819a33e61435ee595848b7ce5e9f3e208da174f
moshe nahmias <moshegrey@gmail.com>
2017-10-29 14:49:33 +0200

    adding fields to owners

--- owners/models.py
+++ owners/models.py
@@ -24,6 +24,10 @@ class RightType(Base):
 
 class OwnerDatabase(Base):
     name = models.CharField(max_length=200)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=30, null=True, blank=True)
+    contact = models.CharField(max_length=200, null=True, blank=True)
+    comments = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
