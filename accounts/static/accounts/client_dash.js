379c48ed9261a3f4a5f6c95f6a5707c5d225ea7a
moshe nahmias <moshegrey@gmail.com>
2017-10-30 17:05:02 +0200

    fixing bug with client dash

--- accounts/static/accounts/client_dash.js
+++ accounts/static/accounts/client_dash.js
@@ -1,13 +1,4 @@
 $('#edit_order').click(function () {
-    $('#link_inquiry').trigger('click');
     $('#license-data').hide();
     $('#license-form').show();
-});
-
-$('.nav.nav-pills.nav-justified>li>a').click(function (e) {
-    if (e.target.id === 'link_inquiry') {
-        $('#license-data').show();
-    } else {
-        $('#pilljustified1').hide();
-    }
 });
