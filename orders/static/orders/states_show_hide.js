11111860102b23469de2e4318dae8170a685cb83
moshe nahmias <moshegrey@gmail.com>
2017-10-30 19:29:06 +0200

    profit is default on javascript

--- orders/static/orders/states_show_hide.js
+++ orders/static/orders/states_show_hide.js
@@ -26,7 +26,7 @@ if (territory_elem.val() === 'United States') {
     $('#usa_states').hide();
 }
 
-if (is_non_profit.val() !== 'selected') {
+if (is_non_profit.val() === 'selected') {
     $('#non-profit').show();
 } else {
     $('#non-profit').hide();
